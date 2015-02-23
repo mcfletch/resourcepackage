a1bbeba2b51a52f62279ab57d588131bed751cd8
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-02-23 00:58:12 -0500

    Move to git and github to do what little work we do on resource package now

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Automated embedding of resources in Python Packages/Modules
+
+ResourcePackage provides the Python programmer with a
+mechanism for automatically maintaining a collection of
+resources embedded in Python source-code modules.
+
+The basic approach taken is to embed one resource in
+each python module of a package explicitly intended
+to be a resource-only package.
+
+See the doc sub-directory for documentation
+"""
+__version__ = (1,0,1)
+__author__ = "Michael Colin Fletcher"
+__license__ = "BSD-Style, see license.txt for details"
+
